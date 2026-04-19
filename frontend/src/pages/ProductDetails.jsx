ed98db71aa674197d41098dd19ecf24c33b0694d
Salmankhan0518 <salmansharif0518@gmail.com>
2026-04-19 13:35:08 +0500

    my fist commit

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function ProductDetails() {
-  return (
-    <div>ProductDetails</div>
-  )
+  return <div>ProductDetails</div>;
 }

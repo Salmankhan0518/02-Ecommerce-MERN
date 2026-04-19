4301462b922caf6fdf0af4edbe8a9c1b76dce176
Salmankhan0518 <salmansharif0518@gmail.com>
2026-04-19 16:11:44 +0500

    complete setup of backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,30 +2,28 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 
 export const signupUser = async (req, res) => {
-        try {
-            const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-            // Check if user already exist 
-            const userExists = await User.findOne({ email })
+    // Check if user already exist
+    const userExists = await User.findOne({ email });
 
-            if(userExists) {
-                return res.status(400).json({ message: "User already exists" })
-            }
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-            //Hash password
-            const hashPassword = await bcrypt.hash(password, 10)
+    //Hash password
+    const hashPassword = await bcrypt.hash(password, 10);
 
-            //Create User
-            await User.create({
-                name, 
-                email,
-                password: hashPassword
-            });
+    //Create User
+    await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
 
-            res.status(201).json({ message: "User created Successfully" })
-
-
-        } catch (error) {
-            res.status(500).json({ message: "Server error", error })
-        }
-}
+    res.status(201).json({ message: "User created Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js"
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -9,13 +10,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
-app.get('/', (req, res) => {
-    res.send("Api is running.............");
-})
+app.get("/", (req, res) => {
+  res.send("Api is running.............");
+});
 
 connectDB();
 
 app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+  console.log("Server is running on port 5001");
 });

82f1966229c4f1926797bd95bdf96b74506d1f54
Salmankhan0518 <salmansharif0518@gmail.com>
2026-04-19 14:53:55 +0500

    create User Model, userSchema

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("DB Connected:", conn.connection.host);
+    console.log("MongoDB Connected Successfully:", conn.connection.host);
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }

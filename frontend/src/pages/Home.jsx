623721a13e52157359eac29fbef4ced262182b3c
Salmankhan0518 <salmansharif0518@gmail.com>
2026-05-06 22:30:28 +0500

    Display products

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+import { Link } from "react-router";
+
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("");
+
+  const loadProducts = async () => {
+    const res = await api.get(`/products?search=${search}&category=${category}`)
+    setProducts(res.data);
+  }
+
+  useEffect(() => {
+    loadProducts();
+  },[search, category]);
+
   return (
-    <div>Home</div>
+    <div className="p-6">
+
+        {/*  Search  */}
+        <div className="mb-4 flex gap-3">
+            <input
+            placeholder="Search Products.."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="border p-2 flex-1"
+            />
+            {/*  Category  */}
+            <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="border px-3 py-2 rounded">
+                <option value="">All Category</option>
+                <option value="Laptops">Laptops</option>
+                <option value="Mobile">Mobile</option>
+                <option value="Tablets">Tablets</option>
+            </select>
+        </div>
+
+        {/*  Product Grid  */}
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5"> 
+          {
+            products.map((product) => (
+            <Link 
+            key={product._id}
+            to={`product/${product._id}`}
+            className="border p-3 rounded shadow hover:shadow-lg transition">
+                <img
+                src={product.image}
+                alt={product.title}
+                className="w-full h-40 object-contain bg-white rounded"
+                />
+                <h2 className="mt-2 font-semibold text-lg">{product.title}</h2>
+                <p className="text-gray-600">${product.price}</p>
+            </Link>
+            ))
+          }
+
+        </div>
+    </div>
   )
 }

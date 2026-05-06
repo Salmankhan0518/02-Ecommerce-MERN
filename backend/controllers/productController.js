27f159c82b19f0b183a2fc5e198a428efa06f066
Salmankhan0518 <salmansharif0518@gmail.com>
2026-05-06 21:28:31 +0500

    complete changes on backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,7 +17,19 @@ export const createProduct = async (req, res) => {
 //Get all products
 export const getProducts = async (req, res) => {
     try {
-        const products = await Product.find().sort({ createdAt: -1 });
+        const { search, category } = req.query;
+
+        let filter = {};
+
+        if (search) {
+            filter.title = { $regex: search, $options: 'i'}
+        }
+
+        if(category) {
+            filter.category = category;
+        }
+
+        const products = await Product.find(filter).sort({ createdAt: -1 });
         res.json(products)
     } catch (error) {
         console.log(error);

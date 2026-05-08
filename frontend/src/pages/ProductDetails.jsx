2328e738de0a783835eaf39b44930705fae69a79
Salmankhan0518 <salmansharif0518@gmail.com>
2026-05-08 17:29:18 +0500

    complete product Details

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import api from "../api/axios";
+import { useParams } from "react-router";
+
 
 export default function ProductDetails() {
-  return <div>ProductDetails</div>;
+  const {id} = useParams()
+  const [product, setProduct] = useState(null);
+
+  const loadProduct = async () => {
+    const res = await api.get("/products/");
+    const p = res.data.find((item) => item._id == id);
+    setProduct(p);
+  };
+
+  useEffect(() => {
+    loadProduct()
+  },[])
+
+  if(!product) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div className="p-6 max-w-3xl mx-auto">
+      <img src={product.image} alt={product.name} className="w-full h-auto mb-4 rounded" />
+      <h1 className="text-2xl font-boldmt-4">{product.title}</h1>
+      <p className="text-gray-700 mt-2">{product.description}</p>
+      <p className="text-xl font-semibold mt-4">${product.price}</p>
+      <button className="mt-6 px-4 py-2 bg-blue-600 text-white rounded  hover:bg-blue-700">Add to Cart</button>
+    </div>
+  )
 }
